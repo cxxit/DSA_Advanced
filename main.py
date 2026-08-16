c3cf45ae54457e84482b76b0d4f7f8e98888a12c
Chin Xu Xin <xchi0019@student.monash.edu>
2026-08-16 19:30:49 +0800

    feat: add main script for ArrayList demonstration

---
+++ main.py
@@ -0,0 +1,23 @@
+from _01_data_structures.array_list import ArrayList
+
+array = ArrayList(4)
+
+# print(array)
+# print(len(array))
+array.append(1)
+# print(array)
+# print(len(array))
+
+array.append(2)
+# print(array)
+array.append(3)
+# print(array)
+# print("length",len(array))
+# print(array)
+
+
+array.append(4)
+# print(array)
+
+array.insert(2,'hehe')
+print(array)
