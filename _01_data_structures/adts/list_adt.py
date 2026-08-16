a40acedc005e47fd8ff1c208eea2b74cbce48f0e
Chin Xu Xin <xchi0019@student.monash.edu>
2026-08-16 19:30:49 +0800

    refactor: move referential_array and update List ADT

---
+++ _01_data_structures/adts/list_adt.py
@@ -0,0 +1,62 @@
+"""
+List ADT.
+
+Reference:
+FIT3155 lecture materials, Monash University.
+"""
+from abc import ABC, abstractmethod
+from typing import TypeVar, Generic
+T = TypeVar('T')
+
+class List(ABC, Generic[T]): 
+    def __init__(self) -> None: 
+        self.length = 0 
+
+    @abstractmethod
+    def __setitem__(self, index: int, item: T) -> None: 
+        pass 
+
+    @abstractmethod
+    def __getitem__(self, index: int) -> T: 
+        pass 
+
+    @abstractmethod
+    def append(self, item: T) -> None: 
+        pass 
+
+    @abstractmethod 
+    def insert(self, index: int, item: T) -> None: 
+        pass
+
+    @abstractmethod
+    def delete_at_index(self, index: int) -> T:
+        pass 
+
+    @abstractmethod
+    def index(self, item: T) -> int: 
+        pass
+
+    def remove(self, item: T) -> None:
+        index = self.index(item)
+        self.delete_at_index(index)
+
+
+    def __len__(self) -> int: 
+        return self.length
+
+    def is_empty(self) -> bool: 
+        return len(self) == 0
+
+    def clear(self): 
+        self.length = 0
+
+
+    def __str__(self) -> str: 
+        ret_str = "["
+        for i, item in enumerate(self.array):
+            ret_str += str(item)
+            ret_str += ", "
+            
+        ret_str = ret_str[:-2] + "]"
+        return ret_str
+
